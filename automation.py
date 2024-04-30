3abeebbfc6c8b31fb62f61198fd7060df456c576
Mel-Rocha <rochamel73@gmail.com>
2024-04-30 10:35:19 -0300

    try login with google - failed

--- automation.py
+++ automation.py
@@ -5,13 +5,12 @@ import logging
 
 from dotenv import load_dotenv
 from selenium import webdriver
-from selenium.webdriver import Keys
 from ratelimit import limits, sleep_and_retry
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.proxy import Proxy, ProxyType
-from selenium.common import TimeoutException, NoSuchElementException
 
 
 load_dotenv()
@@ -25,6 +24,11 @@ class Automation:
         self.driver = None
         self.api_keys = [os.getenv("APIKEY_1"), os.getenv("APIKEY_2")]
 
+    def get_driver_ip(self):
+        self.driver.get('https://api.ipify.org')
+        ip_element = self.driver.find_element(By.TAG_NAME, 'body')
+        return ip_element.text
+
     def start_driver(self):
 
         user_agents = [
@@ -36,20 +40,21 @@ class Automation:
         ]
 
         api_key = random.choice(self.api_keys)
-
         proxy = f"http://scraperapi:{api_key}@proxy-server.scraperapi.com:8001"
         selenium_proxy = Proxy()
         selenium_proxy.proxy_type = ProxyType.MANUAL
         selenium_proxy.http_proxy = proxy
         selenium_proxy.ssl_proxy = proxy
 
+        user_data_path = 'C:\\Users\\Backup 01\\AppData\\Local\\Google\\Chrome\\User Data\\Default'
         chrome_options = Options()
-        chrome_options.add_argument("--incognito")
         chrome_options.add_argument("--disable-blink-features=AutomationControlled")
         chrome_options.add_argument(f'user-agent={random.choice(user_agents)}')
         chrome_options.add_argument('start-maximized')
         chrome_options.add_argument("--enable-features=NetworkService,NetworkServiceInProcess")
         chrome_options.add_argument("--enable-javascript")
+        chrome_options.add_argument("--profile-directory=Default")
+        chrome_options.add_argument(f'--user-data-dir={user_data_path}')
 
         chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
         chrome_options.add_experimental_option('useAutomationExtension', False)
@@ -60,41 +65,32 @@ class Automation:
 
         self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
+        driver_ip = self.get_driver_ip()
+        logging.info(f'Proxy: {proxy}, WebDriver IP: {driver_ip}')
+
     def stop_driver(self):
         if self.driver:
             self.driver.quit()
 
     @sleep_and_retry
     @limits(calls=1, period=60)
-    def access_site(self):
+    def login(self):
         self.start_driver()
 
         try:
-            self.driver.get("https://www.ikesaki.com.br/")
+            self.driver.get("https://www.webmotors.com.br/login")
 
-            time.sleep(random.uniform(8, 10))
+            time.sleep(random.uniform(2, 4))
 
-            def dynamic_page():
-                """
-                To interact with elements that are dynamically loaded or modified
-                with JavaScript prefer to use selenium. Example of use:
-                clicking buttons, filling out forms, waiting for JavaScript to load.
-                """
+            login_with_chrome_xpath = '//*[@id="app"]/div/div[3]/div[1]/div/div[1]/section[1]/div/button[2]/p'
 
-                try:
-                    search_box_xpath = '//*[@id="downshift-0-input"]'
-                    search_box = self.driver.find_element('xpath', search_box_xpath)
-                    search_box.send_keys("Pesquisa", Keys.ENTER)
-                    time.sleep(5)
+            login_with_chrome = self.driver.find_element(By.XPATH, login_with_chrome_xpath)
+            login_with_chrome.click()
+            logging.info("Login realizado com sucesso.")
+            time.sleep(20)
 
-                except TimeoutException as e:
-                    print(f"Erro ao clicar no elemento: Tempo de execução limite {e}")
-                    logging.error("O elemento foi encontrado, mas não se tornou visivel dentro do tempo limite.")
-                except NoSuchElementException as e:
-                    print(f"Erro ao clicar no elemento: Elemento não encontrado {e}.")
-                    logging.error("O elemento não foi encontrado.")
-
-            dynamic_page()
+        except Exception as e:
+            logging.error(f"Erro durante o login: {e}")
 
         finally:
             self.stop_driver()
@@ -102,4 +98,4 @@ class Automation:
 
 if __name__ == "__main__":
     automation = Automation()
-    automation.access_site()
+    automation.login()
